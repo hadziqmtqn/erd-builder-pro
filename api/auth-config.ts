e2cf39bd9c29195a291c003279bafd7888edf31c
hadziqmtqn <hadziqwebid@gmail.com>
2026-04-05 07:13:38 +0700

    refactor: migrate server logic to dedicated directory and remove legacy edge API routes

--- api/auth-config.ts
+++
@@ -1,8 +0,0 @@
-export default async function handler() {
-  return new Response(JSON.stringify({ supabaseAuth: true }), {
-    status: 200,
-    headers: {
-      "content-type": "application/json",
-    },
-  });
-}
